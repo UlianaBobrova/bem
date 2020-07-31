5bf61030714cd57720a946ef1c7543e2b8619810
BOBROVA_ULIANA <uliana.bobrova@gmail.com>
2020-07-31 23:00:09 +0300

    create:modal windows and adaptive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 
 $(document).ready(function () {
-  var menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', () => {
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
+
+  // document.addEventListener('keydown', (event) => {
+  //   console.log(event);
+  // });
+
+  var menuButton = $('.menu-button');
+menuButton.on('click', () => {
+  $('.navbar-bottom').toggleClass('navbar-bottom--visible');
 });
 
 
@@ -105,7 +110,34 @@ var hotelSlider = new Swiper('.hotel-slider', {
   
 
   $('.parallax-window').parallax({imageSrc: '../img/newsletter.jpg'});
-// jquerry будет искать элементы с data-toggle-modal
-var modalButton = $('[data-toggle-modal]')
 
+// jquerry будет искать элементы с data-toggle-modal
+var modalButton = $("[data-toggle = modal]");
+var closeModalButton = $(".modal__close");
+
+// on - как addEventListener
+modalButton.on("click", openModal);
+closeModalButton.on("click", closeModal);
+
+var modalEscWindow = document.querySelector('#booking-modal');
+console.log(modalEscWindow);
+
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode == 27)
+  closeModal(event);
+});
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
 });
